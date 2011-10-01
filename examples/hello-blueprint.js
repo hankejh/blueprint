56eb8d0bda99a5939063d6140f7daa0181ab0bd7
Edward Hotchkiss <e@ingk.com>
2011-10-01 13:47:40 -0400

    response.write()

--- examples/hello-blueprint.js
+++ examples/hello-blueprint.js
@@ -2,8 +2,7 @@
 var blueprint = require("../");
 
 blueprint.get("/", function(request, response) {
-	response.writeHead(200, { "Content-Type" : "text/html" });
-	response.end("<h1>hello blueprint</h1>");
+	response.write("<h1>hello blueprint</h1>");
 });
 
 blueprint.boot(80);
