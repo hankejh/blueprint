cea3dd87aa548f97abb154d06b1205b9430418de
Edward Hotchkiss <e@ingk.com>
2011-11-07 11:03:41 -0500

    tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,34 @@
 
-// core
+/*!
+
+  blueprint::tests
+
+*/
+
 var vows = require("vows");
 var assert = require("assert");
 var should = require("should");
 var request = require("request");
 var http = require("http");
 
-// our blueprint current
 var blueprint = new require("../");
 
-// basic tests
 vows.describe("Blueprint Instantiation Tests").addBatch({
   "When we instantiate blueprint.createServer" : {
     topic : function () { 
-      blueprint.createServer();
-      return blueprint.server;
+      return blueprint;
     },
-    "blueprint.server is an instance of http.Server()" : function (topic) {
+    "blueprint is an instance of http.Server()" : function (topic) {
       topic.should.be.an.instanceof(http.Server);
     }
   },
   "When we .boot blueprint and request /data" : {
     topic : function() {
-      // setup
-      blueprint.createServer();
       blueprint.get("/", function(request, response) {
         response.send("<h1>hello blueprint</h1>");
       });
-      blueprint.boot(8080);
-      // request
-      request("http://localhost/", this.callback);
+      blueprint.boot();
+      request("http://localhost:8000/", this.callback);
     },
     "we should get JSON back with a message of 'hello' and no error"  : function(error, response, body) {
       assert.isNull(error);
