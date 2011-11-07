a86024c506dace3c58663fc1d685d2cc9802fe72
Edward Hotchkiss <e@ingk.com>
2011-11-07 17:44:22 -0500

    bin file for generating skeletons started

--- bin/blueprint.js
+++ bin/blueprint.js
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 
+require("colors");
 var fs = require("fs");
+var ARGS = process.argv.slice(2);
 
+function generate(name) {
+  console.log(name);
+};
 
+switch (ARGS[0]) {
+  case "--help":
+    console.log("USAGE: blueprint <yourappname>".magenta);
+    break;
+  default:
+    generate(ARGS[0]);
+    break;
+};
 
 /* EOF */
