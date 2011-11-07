ce59a47b94e42bb01e204b3809945730b38d3b89
Edward Hotchkiss <e@ingk.com>
2011-11-07 18:12:05 -0500

    bin copy setup, ready for regexp

--- bin/blueprint.js
+++ bin/blueprint.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 
 require("colors");
-var fs = require("fs");
-var ARGS = process.argv.slice(2);
+var path = require("path");
+var wrench = require("wrench");
+var arguments = process.argv.slice(2);
 
 function generate(name) {
-  console.log(name);
+  var source = path.normalize(__dirname + "/../skeleton");
+  var destination = path.normalize(__dirname+"/../"+name);
+  console.log(source+"\n"+destination);
+  wrench.copyDirSyncRecursive(source, destination);
 };
 
-switch (ARGS[0]) {
+switch(arguments[0]) {
   case "--help":
     console.log("USAGE: blueprint <yourappname>".magenta);
     break;
   default:
-    generate(ARGS[0]);
+    generate(arguments[0]);
     break;
 };
 
