e9d2315ed483e89bd08619c8855f9f657c2c049b
Edward Hotchkiss <e@ingk.com>
2011-11-13 14:17:25 -0500

    child process for using tests based on /skeleton, sick

--- skeleton/test/index.test.js
+++ skeleton/test/index.test.js
@@ -2,6 +2,7 @@
 /*!
 
   blueprint::tests
+  node -e 'require("child_process").spawn("node", ["app.js"]).stdout.pipe(process.stdout)
 
 */
 
@@ -12,13 +13,25 @@ var should = require("should");
 var request = require("request");
 var blueprint = require("blueprint");
 
-vows.describe("Blueprint Instantiation Tests").addBatch({
-  "When we instantiate blueprint.createServer" : {
+vows.describe("blueprint instantiation tests").addBatch({
+  "when we instantiate blueprint.createServer" : {
     topic : function () { 
       return blueprint;
     },
     "blueprint is an instance of http.Server()" : function (topic) {
       topic.should.be.an.instanceof(http.Server);
+    }
+  },
+  "when we create a child process for /yourappname/app.js" : {
+    topic : function() {
+      var spawn = require("child_process").spawn;
+      var child = spawn("node", ["app.js"]);
+      request("http://localhost:8000/", this.callback)
+    },
+    "we should get back a message with the text, no error and a status code of 200" : function(error, response, body) {
+      assert.isNull(error);
+      assert.equal(response.statusCode, 200);
+      assert.equal(typeof(body), "string");
     }
   }
 }).export(module);
