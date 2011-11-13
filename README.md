03f6f18d546b73d67abb4567bfbb505ea088ddd9
Edward Hotchkiss <e@ingk.com>
2011-11-13 13:31:01 -0500

    tests were borking due to the sensitive directory structure of blueprint, dependency versions updated

--- README.md
+++ README.md
@@ -9,7 +9,5 @@ $ [sudo] npm install -g blueprint
 $ blueprint yournewapp
 $ cd yournewapp
 $ npm install
-$ echo "running tests..."
-$ make
 $ node app.js
 ```
--- skeleton/app.js
+++ skeleton/app.js
@@ -5,12 +5,13 @@
 
 */
 
+// LOAD CONFIGURATION
 var nconf = require("nconf");
+nconf.use("file", { file: "./config/main.json" });
+
+// SETUP BLUEPRINT W/ MONGOOSE
 var mongoose = require("mongoose");
 var blueprint = require("blueprint");
-
-// LOAD CONFIGURATION
-nconf.use("file", { file: "./config/main.json" });
 
 // CONNECT TO MONGO
 mongoose.connect(nconf.get("mongodb"));
