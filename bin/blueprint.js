d2b408bcd19e9f73dd785f6383284286ba257543
Edward Hotchkiss <e@ingk.com>
2011-11-23 15:19:40 -0500

    scaffolding started

--- bin/blueprint.js
+++ bin/blueprint.js
@@ -1,1 +1,24 @@
 #!/usr/bin/env node
+
+var blueprint = require("../lib/blueprint");
+var scaffold = blueprint.scaffold;
+
+var args = process.argv.slice(2);
+var command = args.shift();
+
+if (command === undefined) {
+
+  return false;
+};
+
+switch(command) {
+  case "help":
+    scaffold.usage();
+    break
+  case "--help":
+    scaffold.usage();
+    break;
+  default:
+    scaffold.usage();
+    break;
+}
